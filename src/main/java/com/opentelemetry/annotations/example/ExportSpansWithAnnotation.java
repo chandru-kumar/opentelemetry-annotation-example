ca55d66c89afb4e662d6d3e5e6bccf64ff4182be
Chandru Kumar R <616076@cognizant.com>
2021-06-10 15:06:59 +0530

    Added console output

--- src/main/java/com/opentelemetry/annotations/example/ExportSpansWithAnnotation.java
+++ src/main/java/com/opentelemetry/annotations/example/ExportSpansWithAnnotation.java
@@ -19,6 +19,7 @@ public final class ExportSpansWithAnnotation {
 	public static void main(String[] args) throws InterruptedException {
 
 		login();
+		System.out.println("Exported Traces!");
 
 	}
 
